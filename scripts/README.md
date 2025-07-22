86a65183f42bf4e68195fae7757da5cb9589ac9d
Adam English <ACEnglish@gmail.com>
2025-07-22 16:46:37 -0400

    code/doc clean

--- scripts/README.md
+++ scripts/README.md
@@ -11,13 +11,13 @@ The `--output` report has columns:
 | -------- | ------------------------------------------------------------------ |
 | feature  | Stratification feature (e.g. `svtype`)                             |
 | value    | Stratification value (e.g. `DEL`)                                  |
-| pval     | Raw permutation test p-value                                       |
 | acc      | Accuracy of calls in this stratification (e.g. `TP / (TP+FN)`)     |
 | delta    | Observed mean ranking minus permutation tests' mean ranking        |
 | q1       | 1% Quantile of permutation tests                                   |
 | q50      | 50% Quantile of permutation tests                                  |
 | q99      | 99% Quantile of permutation tests                                  |
 | obs      | Number of variants observed in this stratification                 |
+| pval     | Raw permutation test p-value                                       |
 | adj_pval | Benjamini/Hochberg multiple test correction adjusted pvalue        |
 | reject   | True or False based on rejection of null hypothesis at given alpha |
 
@@ -29,19 +29,19 @@ python surbscore.py --preset mims bench_result_dir/
 ### How to interpret surbscore output
 As an example, consider the below, truncated surbscore report
 
-| feature	| value           | pval   | acc    | delta     | q1       | q50     | q99     | obs   | adj_pval | reject |
-| --------- | --------------- | ------ | ------ | --------- | -------- | ------- | ------- | ----- | -------- | ------ |
-| VAF_bin   | SomaticLow(<5%) | 0.0    | 0.140  | -111.7586 | -22.653  | -0.0418 | 22.6734 | 13184 | 0.0      | True   |
-| isolated  | False           | 0.0    | 0.391  | -49.437   | -20.6976 | 0.0522  | 21.1852 | 12401 | 0.0      | True   |
-| svtype    | INS             | 0.0182 | 0.4985 | -18.8122  | -21.0539 | -0.0179 | 21.2513 | 20545 | 0.1213   | False  |
-| szbin     | [200,300)       | 0.5651 | 0.5299 | 2.3111    | -31.8288 | -0.0405 | 31.6662 | 2546  | 1.0      | False  |
-| svtype    | DEL             | 0.9806 | 0.6264 | 18.8122   | -21.1796 | -0.0359 | 21.0722 | 13595 | 1.0      | False  |
-| VAF_bin   | GermlineHom     | 1.0    | 0.9446 | 72.259    | -24.9471 | -0.0244 | 24.533  | 5308  | 1.0      | False  |
+| feature	| value           | acc    | delta     | q1       | q50     | q99     | obs   | pval   | adj_pval | reject |
+| --------- | --------------- | ------ | --------- | -------- | ------- | ------- | ----- | ------ | -------- | ------ |
+| VAF_bin   | SomaticLow(<5%) | 0.140  | -111.7586 | -22.653  | -0.0418 | 22.6734 | 13184 | 0.0    | 0.0      | True   |
+| isolated  | False           | 0.391  | -49.437   | -20.6976 | 0.0522  | 21.1852 | 12401 | 0.0    | 0.0      | True   |
+| svtype    | INS             | 0.4985 | -18.8122  | -21.0539 | -0.0179 | 21.2513 | 20545 | 0.0182 | 0.1213   | False  |
+| szbin     | [200,300)       | 0.5299 | 2.3111    | -31.8288 | -0.0405 | 31.6662 | 2546  | 0.5651 | 1.0      | False  |
+| svtype    | DEL             | 0.6264 | 18.8122   | -21.1796 | -0.0359 | 21.0722 | 13595 | 0.9806 | 1.0      | False  |
+| VAF_bin   | GermlineHom     | 0.9446 | 72.259    | -24.9471 | -0.0244 | 24.533  | 5308  | 1.0    | 1.0      | False  |
 
 This report tells us that from top-to-bottom (lowest to highest p-value), we have ranked features on which the caller's
-accurac is most dependent. For example, the top hit of `VAF_bin SomaticLow(<5%)` indicates that the
+accuracy is most dependent. For example, the top hit of `VAF_bin SomaticLow(<5%)` indicates that the
 average accuracy of variants with this feature is `0.140`, which is significantly lower than the average performance of
-the caller across other feature/value stratifications. Furthermore, because the multiple-test correction adjusted
+the caller across other values in this stratification. Furthermore, because the multiple-test correction adjusted
 p-value (`adj_pval`) is still below our alpha (`0.01`), we can reject the null hypothesis that the caller's performance is
 independent of this stratification.
 
--- scripts/surbscore.py
+++ scripts/surbscore.py
@@ -7,11 +7,12 @@ import truvari
 from truvari.vcf2df import vcf2df_main
 import numpy as np
 import pandas as pd
-# requires scipy==1.10.1 and statsmodels==0.13.5 
+# requires scipy==1.10.1 and statsmodels==0.13.5
 from scipy.stats import chi2_contingency
 from statsmodels.stats.multitest import multipletests
 
-def get_scores(df, state="state", features=["svtype","szbin"], min_obs=10):
+
+def get_scores(df, state="state", features=["svtype", "szbin"], min_obs=10):
     """
     Actively filter 0 in either - has to be something
     """
@@ -24,32 +25,37 @@ def get_scores(df, state="state", features=["svtype","szbin"], min_obs=10):
         tot_counts.append(cnt)
     tot_counts = pd.concat(tot_counts)
     tot_counts = tot_counts.reset_index().set_index(['feat', 'index'])
-    
+
     logging.debug("Full Counts\n%s", str(tot_counts))
     all_cont_obs = pd.crosstab(df[state], [df[i] for i in features])
+    logging.info("%d stratifications had ≥1 observed variant",
+                 all_cont_obs.shape[1])
     view = pd.DataFrame(all_cont_obs.sum(axis=0))
     filtered = view[0] < min_obs
     if filtered.any():
-        logging.warning("%d bins with < %d observations ignored.", filtered.sum(), min_obs)
+        logging.warning(
+            "%d stratifications with < %d observations ignored", filtered.sum(), min_obs)
         with pd.option_context('display.max_rows', 100):
             logging.debug("Fitered Bins\n%s", str(all_cont_obs.T[filtered]))
-    
+
     cont_obs = all_cont_obs.T[~filtered].T
-    
+
     chi, pval, dof, exp = chi2_contingency(cont_obs)
     score = (np.sign(cont_obs.values[1] - exp[1])) * \
-            (cont_obs.values[1] - exp[1] )**2 / exp[1]
-    
-    df_rank = pd.DataFrame(list(zip(list(cont_obs.columns), 
-                                     list(cont_obs.values[1]), list(exp[1]), list(score))), 
-                            columns=["Values", "observed", "expected", "score"])
+            (cont_obs.values[1] - exp[1])**2 / exp[1]
+
+    df_rank = pd.DataFrame(list(zip(list(cont_obs.columns),
+                                    list(cont_obs.values[1]), list(exp[1]), list(score))),
+                           columns=["Values", "observed", "expected", "score"])
 
     df_rank.sort_values(["score"], ascending=True, inplace=True)
     df_rank.reset_index(inplace=True, drop=True)
     df_rank.index.names = ['rank']
-    df_rank[features] = pd.DataFrame(df_rank["Values"].tolist(), index=df_rank.index)
+    df_rank[features] = pd.DataFrame(
+        df_rank["Values"].tolist(), index=df_rank.index)
     df_rank = df_rank.drop("Values", axis=1)
     return df_rank, tot_counts, all_cont_obs
+
 
 def permutation_test(a_values, b_values, n_samps=10000, tailed="two"):
     """
@@ -62,13 +68,15 @@ def permutation_test(a_values, b_values, n_samps=10000, tailed="two"):
     logging.debug("Case %.2f (%d)", a_values.mean(), len(a_values))
     logging.debug("Control %.2f (%d)", b_values.mean(), len(b_values))
     logging.debug("delta %.2f", delta)
+
     def run_t(pooled, sizeA, sizeB):
         np.random.shuffle(pooled)
         starA = pooled[:sizeA]
         starB = pooled[-sizeB:]
         return starA.mean() - starB.mean()
     pooled = np.hstack([a_values, b_values])
-    estimates = np.array([run_t(pooled, a_values.size, b_values.size) for _ in range(n_samps)])
+    estimates = np.array(
+        [run_t(pooled, a_values.size, b_values.size) for _ in range(n_samps)])
     if tailed == "left":
         diffCount = len(np.where(estimates <= delta)[0])
     elif tailed == "right":
@@ -78,9 +86,11 @@ def permutation_test(a_values, b_values, n_samps=10000, tailed="two"):
         diffCount = len(np.where(estimates >= delta)[0])
     pct_extreme = float(diffCount) / float(n_samps)
     one, fif, nine = np.quantile(estimates, [0.01, 0.5, 0.99])
-    logging.debug("Quantiles: 1% {:.10f} - 50% {:.10f} - 99% {:.10f}".format(one, fif, nine))
+    logging.debug(
+        "Quantiles: 1% {:.10f} - 50% {:.10f} - 99% {:.10f}".format(one, fif, nine))
     logging.debug("Pct at least as extreme %.2f", pct_extreme)
     return pct_extreme, delta, one, fif, nine
+
 
 def surb_score(m_data, state="state", features=["svtype", "szbin"],  min_obs=10, nperm=10000, tail="left", alpha=0.01):
     """
@@ -95,48 +105,60 @@ def surb_score(m_data, state="state", features=["svtype", "szbin"],  min_obs=10,
             case = np.array(rank[sub].index)
             control = np.array(rank[~sub].index)
             if len(case) < min_obs or len(control) < min_obs:
+                logging.debug("Skipping %s : %s with %d case and %d control observations", feat, val, len(
+                    case), len(control))
                 continue
             logging.debug("Testing %s : %s", feat, val)
-            pval, delta, one, fif, nin = permutation_test(case, control, tailed=tail)
+            pval, delta, one, fif, nin = permutation_test(
+                case, control, tailed=tail)
             acc = m_data[m_data[feat] == val][state].mean()
-            rows.append([feat, val, pval, acc, delta, one, fif, nin, counts.loc[feat, val]["count"]])
+            rows.append([feat, val, acc, delta, one, fif, nin,
+                        counts.loc[feat, val]["count"], pval])
 
-    plt_rank = pd.DataFrame(rows, columns=["feature", "value", "pval", "acc", "delta", "q1", "q50", "q99", "obs"])
-    reject, adj_pval, x, y = multipletests(pvals=plt_rank['pval'], alpha=alpha, method="fdr_bh")
+    plt_rank = pd.DataFrame(rows, columns=[
+                            "feature", "value", "acc", "delta", "q1", "q50", "q99", "obs", "pval"])
+    reject, adj_pval, x, y = multipletests(
+        pvals=plt_rank['pval'], alpha=alpha, method="fdr_bh")
     adj = pd.concat([plt_rank,
                      pd.Series(adj_pval, name="adj_pval"),
                      pd.Series(reject, name="reject"),
                      ], axis=1)
     return adj, all_counts
 
+
 known_features = {'mims': ['svtype', 'szbin', 'TRF', 'isolated', 'VAF_bin']}
+
 
 def parse_args(args):
     parser = argparse.ArgumentParser(prog="surbscore", description=__doc__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("bench", type=str, 
+    parser.add_argument("bench_dir", metavar="DIR", type=str,
                         help="Truvari bench result directory")
-    parser.add_argument("--preset", type=str, default=None, choices=known_features.keys(), 
-                        help="Preset features to score (default:mims)")
     parser.add_argument("--features", type=str, default=None,
-                        help="Comma-separated list of DataFrame column names of features to score.")
+                        help="Comma-separated list of categorical features to score (e.g. svtype,szbin)")
     parser.add_argument("--states", type=str, default='base', choices=['base', 'comp', 'both'],
                         help="Score VCF entries from (base)line, (comp)arison, or (both) (default:base)")
+    parser.add_argument("--preset", type=str, default=None, choices=known_features.keys(),
+                        help="Preset features to score")
     parser.add_argument("-o", "--output", type=str, default="/dev/stdout",
                         help="Score output tsv file (stdout)")
     parser.add_argument("-c", "--count-output", type=str, default=None,
-                        help="Write full observed bin count tsv file (False)")
-    # Maybe group these
-    parser.add_argument("--min-obs", type=int, default=10,
-                        help="Minimum observation for a bin (%(default)s)")
-    parser.add_argument("--nperm", type=int, default=10000,
-                        help="Number of permutations to perform (%(default)s)")
-    parser.add_argument("--tail", type=str, default="left", choices=['left', 'right', 'two'],
-                        help="Tail to test (%(default)s)")
-    parser.add_argument("--alpha", type=truvari.restricted_float, default=0.01, 
-                        help="Significance threshold (%(default)s)")
+                        help="Stratification variant count tsv file")
     parser.add_argument("--debug", action='store_true',
                         help="Verbose logging")
+
+    thresg = parser.add_argument_group("statistics arguments")
+    thresg.add_argument("--min-obs", type=int, default=10,
+                        help="Minimum observation for a bin (%(default)s)")
+    thresg.add_argument("--nperm", type=int, default=10000,
+                        help="Number of permutations to perform (%(default)s)")
+    thresg.add_argument("--tail", type=str, default="left", choices=['left', 'right', 'two'],
+                        help="Tail to test (%(default)s)")
+    thresg.add_argument("--alpha", type=truvari.restricted_float, default=0.01,
+                        help="Significance threshold (%(default)s)")
+    thresg.add_argument("--seed", type=int, default=42,
+                        help="Random seed for permutation tests (%(default)s)")
+
     args = parser.parse_args(args)
     truvari.setup_logging(args.debug, show_version=False)
     if not args.features:
@@ -148,29 +170,28 @@ def parse_args(args):
         args.features = args.features.split(',')
     return args
 
+
 def set_mims_bins(data):
     """
-    Automatic stratification of DataFrame columns based when they contain SMaHT MIMS variants
+    Categorization of DataFrame columns based when they contain SMaHT MIMS annotations
     """
-    def get_vaf_bin(vaf, bins=[0, 0.05, 0.3, 0.7, 1], labels=["SomaticLow(<5%)", "Somatic", "GermlineHet", "GermlineHom"]):
-        """
-        Bin VAFs and apply labels
-        """
-        return pd.cut(vaf, bins=bins, labels=labels)
-
     data['is_tp'] = data['state'].str.startswith('tp')
     data['isolated'] = data['NumNeighbors'] == 0
-    data['VAF_bin'] = get_vaf_bin(data['VAF_alt'])
+    bins = [0, 0.05, 0.3, 0.7, 1]
+    labels = ["SomaticLow(<5%)", "Somatic", "GermlineHet", "GermlineHom"]
+    data['VAF_bin'] = pd.cut(data['VAF_alt'], bins=bins, labels=labels)
+
 
 if __name__ == '__main__':
     """
     Run SurbScore on a truvari directory with benchmarking results
     """
     args = parse_args(sys.argv[1:])
-    data_file = os.path.join(args.bench, "data.jl")
+    np.random.seed(args.seed)
+    data_file = os.path.join(args.bench_dir, "data.jl")
     if not os.path.exists(data_file):
         logging.info("Constructing DataFrame in %s", data_file)
-        vcf2df_main(f"-i -f -b {args.bench} {data_file}".split(' '))
+        vcf2df_main(f"-i -f -b {args.bench_dir} {data_file}".split(' '))
 
     data = joblib.load(data_file)
     if args.preset == "mims":
@@ -180,18 +201,26 @@ if __name__ == '__main__':
         data = data[data['state'].isin(['tpbase', 'fn'])]
     elif args.states == "comp":
         data = data[data['state'].isin(['tp', 'fn'])]
-    
-    logging.info("Scoring Features")
-    scores, counts = surb_score(data, state="is_tp", 
-                        features=args.features, 
-                        min_obs=args.min_obs,
-                        nperm=args.nperm,
-                        tail=args.tail,
-                        alpha=args.alpha,
-                        )
+
+    n_strats = 1
+    n_values = 0
+    for i in args.features:
+        c = data[i].nunique()
+        n_strats *= c
+        n_values += c
+    n_feat = len(args.features)
+    logging.info("Scoring %d features with %d values (max %d stratifications)",
+                 n_feat, n_values, n_strats)
+    scores, counts = surb_score(data, state="is_tp",
+                                features=args.features,
+                                min_obs=args.min_obs,
+                                nperm=args.nperm,
+                                tail=args.tail,
+                                alpha=args.alpha,
+                                )
 
     if args.count_output:
         counts.T.to_csv(args.count_output, sep='\t')
-
-    scores.round(4).sort_values(by=['adj_pval'], ascending=True).to_csv(args.output, sep='\t', index=False)
+    scores.round(4).sort_values(by=['adj_pval', 'pval', 'acc'], ascending=True).to_csv(
+        args.output, sep='\t', index=False)
     logging.info("Finished")
